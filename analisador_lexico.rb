c977d534f07379ae087da865bc5d8edce40a6443
fomoto <fabricio.omoto@gmail.com>
2016-04-01 17:30:54 -0300

    analisador incompleto funcionando

--- analisador_lexico.rb
+++ analisador_lexico.rb
@@ -1,18 +1,28 @@
 require 'pry'
 
 class AnalisadorLexico
-  attr_reader :words, :reserved_words, :numbers, :symbols, :compost_symbols, :tokens
+  attr_reader :letters, :reserved_words, :numbers, :symbols, :compost_symbols, :tokens
   
   def initialize
-    @words = ('a'..'z').to_a
+    @letters = ('a'..'z').to_a
     @reserved_words = ['auto','break','case','char','const','continue','default','do','double','else',
                       'enum','extern','float','for','goto','if','int','long','register','return',
                       'short','signed','sizeof','static','struct','switch','typedef','union','unsigned',
                       'void','volatile','while']
     @numbers = ('0'..'9').to_a
-    @symbols = ['.',';',',','(',')',':','+','<','>','-','*','%']
+    @symbols = ['.',';',',','(',')',':','+','<','>','-','*','%','=']
     @compost_symbols = ['++','--','>=','<=','+=','-=','*=','/=','%=']
     @tokens = []
+  end
+
+  # def valid_letter(pos,line)
+  #   valid = letters + numbers + symbols + compost_symbols + [' ']
+  #   valid.any?{|v| v.include?line[pos-1]}
+  # end
+
+  def valid_word(pos,pos2,line)
+    valid = symbols + compost_symbols + [' ']
+    valid.any?{|v| v.include?line[pos-1]} && valid.any?{|v| v.include?line[pos2+1]}
   end
 
   def analyze_line(line, nline)
@@ -23,27 +33,38 @@ class AnalisadorLexico
         pos += 1
         next
       end
-      if words.include? line[pos] #line[pos] é uma letra?
+      if letters.include? line[pos] #line[pos] é uma letra?
         pos2 = pos
-        !!!!!!!if line[pos+1] == ' ' || line[pos+1].nil?
-          @tokens << Token.new(nline,line[pos],'Letra')
-        els!!!!!if reserved_words.any?{|rw| rw.include? line[pos]}
-          while reserved_words.any?{|rw| rw.include? line[pos..pos2]}
-            if reserved_words.any?{reserved_words.include? line[pos..pos2]}
+        if reserved_words.any?{|rw| rw.include? line[pos]}
+          while reserved_words.any?{|rw| rw.include? line[pos..pos2]} && !line[pos2].nil?
+            if reserved_words.any?{reserved_words.include? line[pos..pos2]} && valid_word(pos,pos2,line)
               @tokens << Token.new(nline,line[pos..pos2],'Palavra Reservada')
-              pos = pos2
+              pos = pos2 + 1
               break
             end
             pos2 += 1
           end
-          @tokens << Token.new(nline, line[pos], 'Letra') if words.include? line[pos]
+          @tokens << Token.new(nline, line[pos], 'Letra') if letters.include? line[pos]
+        else
+          @tokens << Token.new(nline, line[pos], 'Letra') if letters.include? line[pos]
         end
       elsif symbols.include? line[pos]
-        pos += 1
-        next
+        pos2 = pos
+        if compost_symbols.any?{|cs| cs.include? line[pos]}
+          while compost_symbols.any?{|cs| cs.include? line[pos..pos2]} && !line[pos2].nil?
+            if compost_symbols.any?{compost_symbols.include? line[pos..pos2]}
+              @tokens << Token.new(nline,line[pos..pos2],'Simbolo Composto')
+              pos = pos2 + 1
+              break
+            end
+            pos2 += 1
+          end
+          @tokens << Token.new(nline,line[pos],'Simbolo') if symbols.include? line[pos]
+        else
+          @tokens << Token.new(nline,line[pos],'Simbolo') if symbols.include? line[pos]
+        end
       elsif numbers.include? line[pos]
-        pos += 1
-        next
+        @tokens << Token.new(nline,line[pos],'Simbolo')
       end
       pos += 1
     end
